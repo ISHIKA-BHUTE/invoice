1be6a7fc1939cf51828c19882fce57e78e6a1cbc
ISHIKA-BHUTE <ishikabhute01@gmail.com>
2023-06-23 17:32:13 +0530

    done

--- InvoiceProject-newCode/InvoiceGeneration/src/main/java/com/invoice/repository/InvoiceInlineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.invoice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.invoice.models.InvoiceInlineModel;
-
-public interface InvoiceInlineRepository extends JpaRepository<InvoiceInlineModel,Long> {
-
-}
